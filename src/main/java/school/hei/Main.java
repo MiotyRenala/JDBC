95add86fd7ec5b40d0e21539a16d392aaf163a18
mioty <hei.mioty.2@gmail.com>
2025-12-01 23:10:28 +0300

    feat: add main

--- src/main/java/school/hei/Main.java
+++ src/main/java/school/hei/Main.java
@@ -4,14 +4,20 @@ package school.hei;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        throws Exception {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            ApplicationContext context = SpringApplication.run(ConnectionApplication.class, args);
+
+            DatabaseConfig dbConfig = context.getBean(DatabaseConfig.class);
+
+
+            System.out.println("DB URL = " + dbConfig.getDbUrl());
+            System.out.println("DB Username = " + dbConfig.getDbUsername());
+
+            var conn = dbConfig.getConnection();
+            System.out.println("Connection OK ? " + !conn.isClosed());
+            conn.close();
         }
+
     }
 }
