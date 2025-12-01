47d2786fc703ad374d8fa201773f8a6c9166bac6
mioty <hei.mioty.2@gmail.com>
2025-12-01 22:39:16 +0300

    feat: add databaseConfig

--- src/main/java/DatabaseConfig.java
+++ src/main/java/DatabaseConfig.java
@@ -1,5 +1,17 @@
+
+@Component
 public class DatabaseConfig {
-    private String url;
-    private String username;
-    private String password;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}"})
+    private String dbPassword;
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriveManqger, getConnection(this.dbUrl,this.dbUsername; this.dbPassword)
+    }
 }
